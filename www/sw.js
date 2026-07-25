a96128025c2da45e85f26716680f7fa10838e86f
wosatex <wosatex@googlemail.com>
2026-07-26 01:00:25 +0200

    Update sw.js

--- www/sw.js
+++ www/sw.js
@@ -2,7 +2,7 @@
    Hülle aus dem Cache, Daten aus dem Netz mit Cache als Rückfall.
    VERSION bei jeder Änderung an den Dateien unten hochzählen. */
 
-const VERSION = "v1";
+const VERSION = "v2";
 const SHELL = `bw-shell-${VERSION}`;
 const DATA = `bw-data-${VERSION}`;
 
